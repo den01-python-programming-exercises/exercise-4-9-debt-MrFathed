5e76f684a4cdad2c6bc808f8e8c1b1dede10a394
Justin Turner <jmturner638@gmail.com>
2021-08-04 05:30:32 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,19 @@
+from debt import Debt
+
 def main():
     #write your code below this line
+    mortgage = Debt(120000.0, 1.01)
+    mortgage.print_balance()
+
+    mortgage.wait_one_year()
+    mortgage.print_balance()
+
+    years = 0
+    while years < 20:
+        mortgage.wait_one_year()
+        years += 1
+
+    mortgage.print_balance()
 
 if __name__ == '__main__':
     main()
